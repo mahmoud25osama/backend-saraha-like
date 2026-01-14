8d9a3e6fed13c388976472590cbd471aeadbea29
mahmoud25osama <mahmoud4h5@gmail>
2026-01-14 02:53:25 +0200

    update config of vercel

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,14 @@
-import { dbConn } from './database/dbConnection'
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
-import authRoutes from './modules/auth/auth.routes'
-import messageRoutes from './modules/message/message.routes'
-import userRoutes from './modules/user/user.routes'
-import { AppError } from './utils/AppError'
-import { errorHandle } from './middleware/errorHandle'
+import authRoutes from '../modules/auth/auth.routes'
+import messageRoutes from '../modules/message/message.routes'
+import userRoutes from '../modules/user/user.routes'
+import { AppError } from '../utils/AppError'
+import { errorHandle } from '../middleware/errorHandle'
+import ServerlessHttp from 'serverless-http'
+import { dbConn } from '../database/dbConnection'
 
 // Load environment variables
 dotenv.config()
@@ -40,3 +41,4 @@ app.all(/.*/, (req, res, next) => {
 // Error handling middleware
 app.use(errorHandle)
 // Start the server
+export const handler = ServerlessHttp(app)
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import { app } from './app'
+import { app } from './api'
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)

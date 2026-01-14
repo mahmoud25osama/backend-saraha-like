d57fec003ceb24751f43d3adf423b4a78d1e90e9
mahmoud25osama <mahmoud4h5@gmail>
2026-01-15 00:19:13 +0200

    update dbConnection to deploy in vercel

--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -1,18 +1,53 @@
 import mongoose from 'mongoose'
 
-let isConnected = false
+const MONGODB_URI = process.env.MONGODB_URI
 
-export const dbConn = async () => {
-    if (isConnected) return
+if (!MONGODB_URI) {
+    throw new Error(
+        'Please define the MONGODB_URI environment variable inside .env.local / Vercel'
+    )
+}
+
+let cached = (global as any).mongoose
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null }
+}
+
+async function dbConn() {
+    if (cached.conn) {
+        console.log('👌 Using existing MongoDB connection')
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+            serverSelectionTimeoutMS: 5000,
+            maxPoolSize: 10,
+            minPoolSize: 2,
+        }
+
+        cached.promise = mongoose
+            .connect(MONGODB_URI!, opts)
+            .then((mongoose) => {
+                console.log('🔥 New MongoDB connection established')
+                return mongoose
+            })
+            .catch((err) => {
+                cached.promise = null
+                throw err
+            })
+    }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI || '', {
-            dbName: 'saraha-like',
-        })
-        isConnected = true
-        console.log('MongoDB Connected Successfully.')
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error)
-        throw error
+        cached.conn = await cached.promise
+    } catch (e) {
+        cached.promise = null
+        throw e
     }
+
+    return cached.conn
 }
+
+export { dbConn }

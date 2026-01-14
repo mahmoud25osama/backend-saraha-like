eb162fce26989a60da72dac5fd7e3e95a91fc09b
mahmoud25osama <mahmoud4h5@gmail>
2026-01-14 02:16:19 +0200

    update dbConn to singlton to deploy

--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -5,10 +5,14 @@ let isConnected = false
 export const dbConn = async () => {
     if (isConnected) return
 
-    await mongoose.connect(process.env.MONGODB_URI || '', {
-        dbName: 'saraha-like',
-    })
-
-    isConnected = true
-    console.log('MongoDB Connected Successfully.')
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || '', {
+            dbName: 'saraha-like',
+        })
+        isConnected = true
+        console.log('MongoDB Connected Successfully.')
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error)
+        throw error
+    }
 }

fbef059f0198422ecfe2439201f47885ea8b572c
mahmoud25osama <mahmoud4h5@gmail>
2026-01-15 01:21:55 +0200

    add partitioned: true

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -19,6 +19,7 @@ const sendToken = (res: Response, userId: string) => {
         sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         path: '/',
+        partitioned: true,
     })
 }
 
